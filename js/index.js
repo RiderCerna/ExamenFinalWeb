5314090df43a4216cb9ed43d41d4e40ba5422756
Rider Henry Cerna Quispe <113152837+RiderCerna@users.noreply.github.com>
2024-06-30 22:35:23 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function cambiarBanner() {
+    document.getElementById('banner').src = 'img/banner2.fw.png';
+}
+
+function restablecerBanner() {
+    document.getElementById('banner').src = 'img/banner1.fw.png';
+}
